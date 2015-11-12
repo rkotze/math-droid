c838c197c2779c3c5ea03275354188ac03a18b6f
Richard Kotze <rich_taylormade@hotmail.com>
2015-11-12 18:15:19 +0000

    finished check for prime number using square root.

--- prime-numbers/prime-numbers.js
+++ prime-numbers/prime-numbers.js
@@ -3,7 +3,16 @@ module.exports = {
 	check: function(prime){
 		if(prime < 2)
 			return false;
-		
+
+		var sqr = Math.floor(Math.sqrt(prime)),
+		i = 2;
+		while(i <= sqr){
+			if(prime % i === 0)
+				return false;
+
+			i++;
+		}
+
 		return true;
 	}
 };
